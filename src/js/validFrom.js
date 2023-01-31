dc3e056ef16c620ce6d3866e8ecf03b021f98080
kimba <skyqwn@gmail.com>
2023-01-31 13:34:15 +0900

    gym upload validation

--- src/js/validFrom.js
+++ src/js/validFrom.js
@@ -1,59 +1,17 @@
-const signinForm = document.getElementById("signinForm");
-const signupForm = document.getElementById("signupForm");
-const jsPostUploadForm = document.getElementById("jsPostUploadForm");
-const validErrorMessage = document.getElementById("validErrorMessage");
-
 const form = document.querySelector("form");
 
+const validErrorMessage = document.getElementById("validErrorMessage");
+
 const createErrorMessage = (message) => {
-  validErrorMessage.innerHTML = "";
-  validErrorMessage.innerHTML = message || "필수항목을 입력하세요";
-  validErrorMessage.classList.add("validMessage");
+  validErrorMessage.innerHTML = ``;
+  validErrorMessage.innerText = message || "필수항목을 입력하세요";
+  validErrorMessage.classList.add("errorValid");
 };
 
-const signinValid = () => {
-  const emailInput = signinForm.querySelector("input[type=email]");
-  const passwordInput = signinForm.querySelector("input[type=password]");
-  const signinSubmit = signinForm.querySelector("#jsSubmit");
-
-  signinSubmit.addEventListener("click", (e) => {
-    if (!emailInput.value) {
-      emailInput.classList.add("valid");
-      createErrorMessage();
-    }
-    if (!passwordInput.value) {
-      passwordInput.classList.add("valid");
-      createErrorMessage();
-      return;
-    }
-    signinForm.submit();
-  });
-};
-
-const signupValid = () => {
-  const emailInput = signinForm.querySelector("input[type=email]");
-  const passwordInput = signinForm.querySelector("input[type=password]");
-  const signinSubmit = signinForm.querySelector("#jsSubmit");
-
-  signinSubmit.addEventListener("click", (e) => {
-    if (!emailInput.value) {
-      emailInput.classList.add("valid");
-      createErrorMessage();
-    }
-    if (!passwordInput.value) {
-      passwordInput.classList.add("valid");
-      createErrorMessage();
-      return;
-    }
-    signinForm.submit();
-  });
-};
-
-const validEmail = (ele) => {
+const validEmail = (input) => {
   let validRegex =
     /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-
-  if (ele.value.match(validRegex)) {
+  if (input.value.match(validRegex)) {
     return true;
   } else {
     return false;
@@ -63,29 +21,33 @@ const validEmail = (ele) => {
 const formValid = (ele) => {
   const submitBtn = ele.querySelector("#jsSubmit");
 
-  submitBtn.addEventListener("click", () => {
+  const inputs = ele.querySelectorAll("input");
+  const textarea = ele.querySelector("textarea");
+  const select = ele.querySelector("select");
+
+  // GYM 전용 선택자
+
+  const address = ele.querySelector("input[name=address]");
+  const photosInput = ele.querySelector("input[name=photos]");
+  const yearRoundRadio = ele.querySelectorAll("input[name=yearRound]");
+  const onedayRadio = ele.querySelectorAll("input[name=oneday]");
+
+  const emailInput = ele.querySelector("input[type=email]");
+
+  const handleSubmit = () => {
     let ok = true;
-    const inputs = ele.querySelectorAll("input");
-    const textarea = ele.querySelector("textarea");
-    const select = ele.querySelector("select");
-
-    const emailInput = document.querySelector("ele[type=email]");
-
-    if (emailInput && emailInput.value) {
-      const emailBoolean = validEmail(emailInput);
-      if (!emailBoolean) {
-        emailInput.classList.remove("valid");
-        emailInput.classList.add("valid");
-        createErrorMessage("이메일 형식이 아닙니다");
-        emailInput.focus();
-        return;
-      }
-    }
 
     const validRequired = (ele) => {
+      const exceptionName = [
+        "address",
+        "photos",
+        "yearRound",
+        "oneday",
+        "email",
+      ];
+      const name = ele.name;
       const value = ele.value;
       ele.classList.remove("valid");
-
       if (!value) {
         ele.classList.add("valid");
         createErrorMessage();
@@ -93,8 +55,28 @@ const formValid = (ele) => {
       }
     };
 
+    if (emailInput && emailInput.value) {
+      const emailBoolean = validEmail(emailInput);
+      if (!emailBoolean) {
+        createErrorMessage("이메일 형식이 아닙니다");
+        emailInput.classList.remove("valid");
+        emailInput.classList.add("valid");
+        emailInput.focus();
+        return;
+      }
+    }
+
     for (let i = 0; i < inputs.length; i++) {
-      validRequired(inputs[i]);
+      if (
+        inputs[i].name !== "onedayPay" ||
+        inputs[i].name !== "address" ||
+        inputs[i].name !== "location" ||
+        inputs[i].type !== "file" ||
+        inputs[i].type !== "radio" ||
+        inputs[i].type !== "checkbox"
+      ) {
+        validRequired(inputs[i]);
+      }
     }
 
     if (textarea) {
@@ -105,22 +87,79 @@ const formValid = (ele) => {
       validRequired(select);
     }
 
+    if (address) {
+      const value = address.value;
+      const map = document.getElementById("kakaoMap");
+      const searchAddressBtn = document.getElementById("searchAddressBtn");
+      map.classList.remove("valid");
+      searchAddressBtn.style.backgroundColor = "#00b894";
+
+      if (!value) {
+        map.classList.add("valid");
+        searchAddressBtn.style.backgroundColor = "#ff5e57";
+        createErrorMessage("주소를 검색하세요");
+        ok = false;
+      }
+    }
+
+    if (photosInput) {
+      const files = photosInput.files;
+      const btn = document.getElementById("fakeFileBtn");
+      console.log(photosInput);
+      console.log(btn);
+      btn.style.backgroundColor = "#00b894";
+
+      if (files.length < 4) {
+        btn.style.backgroundColor = "#ff5e57";
+        createErrorMessage("사진을 4장 이상 올려주세요");
+        ok = false;
+      }
+    }
+
+    // if (yearRoundRadio.length > 0) {
+    //   let radioOk = false;
+    //   for (let i = 0; i < yearRoundRadio.length; i++) {
+    //     if (yearRoundRadio[i].checked) {
+    //       radioOk = true;
+    //     }
+    //   }
+    //   const labels = document.querySelectorAll(".yearRoundLabel");
+    //   if (!radioOk) {
+    //     for (let i = 0; i < labels.length; i++) {
+    //       labels[i].style.border = "2px solid #ff5e57";
+    //     }
+    //   } else {
+    //     for (let i = 0; i < labels.length; i++) {
+    //       labels[i].style.border = "1px solid #00b894";
+    //     }
+    //   }
+    // }
+
+    // if (onedayRadio.length > 0) {
+    //   let radioOk = false;
+    //   for (let i = 0; i < onedayRadio.length; i++) {
+    //     if (onedayRadio[i].checked) {
+    //       radioOk = true;
+    //     }
+    //   }
+    //   const labels = document.querySelectorAll(".onedayLabel");
+    //   if (!radioOk) {
+    //     for (let i = 0; i < labels.length; i++) {
+    //       labels[i].style.border = "2px solid #ff5e57";
+    //     }
+    //   } else {
+    //     for (let i = 0; i < labels.length; i++) {
+    //       labels[i].style.border = "1px solid #00b894";
+    //     }
+    //   }
+    // }
+
     if (ok) {
       ele.submit();
     }
-  });
+  };
+
+  submitBtn.addEventListener("click", handleSubmit);
 };
-
-// if (signinForm) {
-//   formValid(signinForm);
-// }
-
-// if (signupForm) {
-//   formValid(signupForm);
-// }
-
-// if (jsPostUploadForm) {
-//   formValid(jsPostUploadForm);
-// }
 
 formValid(form);
--- static/js/validFrom.js
+++ static/js/validFrom.js
@@ -1,60 +1,18 @@
 "use strict";
 
-var signinForm = document.getElementById("signinForm");
-var signupForm = document.getElementById("signupForm");
-var jsPostUploadForm = document.getElementById("jsPostUploadForm");
-var validErrorMessage = document.getElementById("validErrorMessage");
-
 var form = document.querySelector("form");
+
+var validErrorMessage = document.getElementById("validErrorMessage");
 
 var createErrorMessage = function createErrorMessage(message) {
   validErrorMessage.innerHTML = "";
-  validErrorMessage.innerHTML = message || "필수항목을 입력하세요";
-  validErrorMessage.classList.add("validMessage");
+  validErrorMessage.innerText = message || "필수항목을 입력하세요";
+  validErrorMessage.classList.add("errorValid");
 };
 
-var signinValid = function signinValid() {
-  var emailInput = signinForm.querySelector("input[type=email]");
-  var passwordInput = signinForm.querySelector("input[type=password]");
-  var signinSubmit = signinForm.querySelector("#jsSubmit");
-
-  signinSubmit.addEventListener("click", function (e) {
-    if (!emailInput.value) {
-      emailInput.classList.add("valid");
-      createErrorMessage();
-    }
-    if (!passwordInput.value) {
-      passwordInput.classList.add("valid");
-      createErrorMessage();
-      return;
-    }
-    signinForm.submit();
-  });
-};
-
-var signupValid = function signupValid() {
-  var emailInput = signinForm.querySelector("input[type=email]");
-  var passwordInput = signinForm.querySelector("input[type=password]");
-  var signinSubmit = signinForm.querySelector("#jsSubmit");
-
-  signinSubmit.addEventListener("click", function (e) {
-    if (!emailInput.value) {
-      emailInput.classList.add("valid");
-      createErrorMessage();
-    }
-    if (!passwordInput.value) {
-      passwordInput.classList.add("valid");
-      createErrorMessage();
-      return;
-    }
-    signinForm.submit();
-  });
-};
-
-var validEmail = function validEmail(ele) {
+var validEmail = function validEmail(input) {
   var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-
-  if (ele.value.match(validRegex)) {
+  if (input.value.match(validRegex)) {
     return true;
   } else {
     return false;
@@ -64,29 +22,27 @@ var validEmail = function validEmail(ele) {
 var formValid = function formValid(ele) {
   var submitBtn = ele.querySelector("#jsSubmit");
 
-  submitBtn.addEventListener("click", function () {
+  var inputs = ele.querySelectorAll("input");
+  var textarea = ele.querySelector("textarea");
+  var select = ele.querySelector("select");
+
+  // GYM 전용 선택자
+
+  var address = ele.querySelector("input[name=address]");
+  var photosInput = ele.querySelector("input[name=photos]");
+  var yearRoundRadio = ele.querySelectorAll("input[name=yearRound]");
+  var onedayRadio = ele.querySelectorAll("input[name=oneday]");
+
+  var emailInput = ele.querySelector("input[type=email]");
+
+  var handleSubmit = function handleSubmit() {
     var ok = true;
-    var inputs = ele.querySelectorAll("input");
-    var textarea = ele.querySelector("textarea");
-    var select = ele.querySelector("select");
-
-    var emailInput = document.querySelector("ele[type=email]");
-
-    if (emailInput && emailInput.value) {
-      var emailBoolean = validEmail(emailInput);
-      if (!emailBoolean) {
-        emailInput.classList.remove("valid");
-        emailInput.classList.add("valid");
-        createErrorMessage("이메일 형식이 아닙니다");
-        emailInput.focus();
-        return;
-      }
-    }
 
     var validRequired = function validRequired(ele) {
+      var exceptionName = ["address", "photos", "yearRound", "oneday", "email"];
+      var name = ele.name;
       var value = ele.value;
       ele.classList.remove("valid");
-
       if (!value) {
         ele.classList.add("valid");
         createErrorMessage();
@@ -94,8 +50,21 @@ var formValid = function formValid(ele) {
       }
     };
 
+    if (emailInput && emailInput.value) {
+      var emailBoolean = validEmail(emailInput);
+      if (!emailBoolean) {
+        createErrorMessage("이메일 형식이 아닙니다");
+        emailInput.classList.remove("valid");
+        emailInput.classList.add("valid");
+        emailInput.focus();
+        return;
+      }
+    }
+
     for (var i = 0; i < inputs.length; i++) {
-      validRequired(inputs[i]);
+      if (inputs[i].name !== "onedayPay" || inputs[i].name !== "address" || inputs[i].name !== "location" || inputs[i].type !== "file" || inputs[i].type !== "radio" || inputs[i].type !== "checkbox") {
+        validRequired(inputs[i]);
+      }
     }
 
     if (textarea) {
@@ -106,22 +75,79 @@ var formValid = function formValid(ele) {
       validRequired(select);
     }
 
+    if (address) {
+      var value = address.value;
+      var map = document.getElementById("kakaoMap");
+      var searchAddressBtn = document.getElementById("searchAddressBtn");
+      map.classList.remove("valid");
+      searchAddressBtn.style.backgroundColor = "#00b894";
+
+      if (!value) {
+        map.classList.add("valid");
+        searchAddressBtn.style.backgroundColor = "#ff5e57";
+        createErrorMessage("주소를 검색하세요");
+        ok = false;
+      }
+    }
+
+    if (photosInput) {
+      var files = photosInput.files;
+      var btn = document.getElementById("fakeFileBtn");
+      console.log(photosInput);
+      console.log(btn);
+      btn.style.backgroundColor = "#00b894";
+
+      if (files.length < 4) {
+        btn.style.backgroundColor = "#ff5e57";
+        createErrorMessage("사진을 4장 이상 올려주세요");
+        ok = false;
+      }
+    }
+
+    // if (yearRoundRadio.length > 0) {
+    //   let radioOk = false;
+    //   for (let i = 0; i < yearRoundRadio.length; i++) {
+    //     if (yearRoundRadio[i].checked) {
+    //       radioOk = true;
+    //     }
+    //   }
+    //   const labels = document.querySelectorAll(".yearRoundLabel");
+    //   if (!radioOk) {
+    //     for (let i = 0; i < labels.length; i++) {
+    //       labels[i].style.border = "2px solid #ff5e57";
+    //     }
+    //   } else {
+    //     for (let i = 0; i < labels.length; i++) {
+    //       labels[i].style.border = "1px solid #00b894";
+    //     }
+    //   }
+    // }
+
+    // if (onedayRadio.length > 0) {
+    //   let radioOk = false;
+    //   for (let i = 0; i < onedayRadio.length; i++) {
+    //     if (onedayRadio[i].checked) {
+    //       radioOk = true;
+    //     }
+    //   }
+    //   const labels = document.querySelectorAll(".onedayLabel");
+    //   if (!radioOk) {
+    //     for (let i = 0; i < labels.length; i++) {
+    //       labels[i].style.border = "2px solid #ff5e57";
+    //     }
+    //   } else {
+    //     for (let i = 0; i < labels.length; i++) {
+    //       labels[i].style.border = "1px solid #00b894";
+    //     }
+    //   }
+    // }
+
     if (ok) {
       ele.submit();
     }
-  });
+  };
+
+  submitBtn.addEventListener("click", handleSubmit);
 };
-
-// if (signinForm) {
-//   formValid(signinForm);
-// }
-
-// if (signupForm) {
-//   formValid(signupForm);
-// }
-
-// if (jsPostUploadForm) {
-//   formValid(jsPostUploadForm);
-// }
 
 formValid(form);
