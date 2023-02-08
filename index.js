33eb196142737cf7852df955968f39ed187d1eb4
kimba <skyqwn@gmail.com>
2023-02-08 16:11:02 +0900

    proxy 재설정

--- index.js
+++ index.js
@@ -96,9 +96,9 @@ app.use(cors(corsOption));
 if (process.env.NODE_ENV === "Developement") {
   app.use(morgan("dev"));
 }
-// if (process.env.NODE_ENV === "Production") {
-//   app.set("trust proxy", 1);
-// }
+if (process.env.NODE_ENV === "Production") {
+  app.set("trust proxy", 1);
+}
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
@@ -107,7 +107,7 @@ app.use(
     secret: process.env.COOKIE_SECRET,
     resave: false,
     saveUninitialized: false,
-    // proxy: true,
+    proxy: true,
     store: MongoStore.create({ mongoUrl }),
     cookie: {
       maxAge: 3.6e6 * 24,
